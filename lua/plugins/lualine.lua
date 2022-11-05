8c1e557a908789b04093e2b9a276e37ece5c024e
Ryzen <antonpotekha@gmail.com>
2022-11-05 18:25:49 +0300

    -Add current venv name to Lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,8 @@
+local function show_venv()
+    local venv = vim.env.VIRTUAL_ENV_PROMPT:gsub("^nil$", "")
+    return venv
+end
+
 require('lualine').setup {
     options = {
         theme = 'auto',
@@ -9,8 +14,11 @@ require('lualine').setup {
     sections = {
         lualine_a = { { 'mode', fmt = function(res) return res:sub(1, 1) end } },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_c = {
+            { show_venv,
+                color = { fg = '#abb1bb', bg = '#373e4d' } }
+        },
+        lualine_x = { 'encoding', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
     },
